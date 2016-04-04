6d1e09cfc02180d4879359e89ba2997db4bddd07
won <wjin0352@gmail.com>
2016-04-04 19:18:38 -0400

    modal feature prototype

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 
+  $(function() {
+    // OPEN---- modal
+    $('[data-popup-open]').on('click', function(e) {
+      var targeted_popup_class = $(this).attr('data-popup-open');
+      $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+      e.preventDefault();
+    });
+
+    // CLOSE---- modal
+    $('[data-popup-close]').on('click', function(e) {
+      var targeted_popup_class = $(this).attr('data-popup-close');
+      $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+      e.preventDefault();
+    });
+  });
 });
 
