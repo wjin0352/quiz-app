9eec4bedbb3c525bda808588157cc498c20c1c47
won <wjin0352@gmail.com>
2016-04-05 14:28:36 -0400

    main content screen width height

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     // OPEN---- modal
     $('[data-popup-open]').on('click', function(e) {
       var targeted_popup_class = $(this).attr('data-popup-open');
-      $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+      $('[data-popup="' + targeted_popup_class + '"]').fadeIn(1000);
 
       e.preventDefault();
     });
@@ -12,7 +12,7 @@ $(document).ready(function() {
     // CLOSE---- modal
     $('[data-popup-close]').on('click', function(e) {
       var targeted_popup_class = $(this).attr('data-popup-close');
-      $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+      $('[data-popup="' + targeted_popup_class + '"]').fadeOut(1000);
 
       e.preventDefault();
     });
